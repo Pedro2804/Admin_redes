ca8a39bc40248357f5633bc41ec2491dfc2070fc
Pedro2804 <pedro28hdez@hotmail.com>
2023-11-22 20:50:10 -0600

    final

--- Spotify/Spotify.py
+++ Spotify/Spotify.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 # Importamos la librería tkinter para la interfaz grafica
 import tkinter as tk
+from tkinter.ttk import *
 from tkinter import messagebox, simpledialog
 # Importamos la libreria urllib.parse para verificar las URL's ingresadas
 from urllib.parse import urlparse
@@ -11,7 +12,7 @@ from pytube import YouTube
 from youtubesearchpython import VideosSearch
 import os
 import requests
-import platform
+import threading
 
 def credentials():
     # Configura las credenciales de la API
@@ -21,6 +22,7 @@ def credentials():
                                                 scope='playlist-read-private'))
 
 def new_token(credential_old):
+    #Actualizar token de acceso
     url = "https://accounts.spotify.com/api/token"
     data = {
         "grant_type": "refresh_token",
@@ -37,18 +39,14 @@ def new_token(credential_old):
 def is_url(url):
     try:
         result = urlparse(url)
-        return all([result.scheme, result.netloc])
+        return result.netloc in {"open.spotify.com"}
     except ValueError:
         return False
     
 def get_URL():
-    if platform.system() == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
     #Pedimos al usuario el link de su playlist de Spotify
+    #text_area.delete("1.0", tk.END)
     playlist_link = textbox.get()
-    text_area.delete("1.0", tk.END)
 
     if(playlist_link != '' and is_url(playlist_link)):
         #Descomponemos la URL a un array, separando al url por '/'
@@ -66,25 +64,20 @@ def get_URL():
             list_name, name_dir = get_name_music(playlist_id)
 
             if list_name != "":
-                i=1
-                limit = len(list_name)
-                print("\nDownloading")
-                bar = bar_progres(0, limit, 50)
-                print(f"{bar}", end = "\r")
+                def down(list_name, name_dir):
+                    i = 0
+                    for name in list_name:
+                        download_music(name, name_dir, i, len(list_name))
+                        i += 1
 
-                for name in list_name:
-                    download_music(name, name_dir)
-                    bar = bar_progres(i, limit, 50)
-                    print(f"{bar}", end = "\r")
-                    i += 1
-
-                textbox.delete(0, tk.END)
-                print("\n\nDownload complete\n")
+                bar['value']=0
+                download_thread = threading.Thread(target=down, args=(list_name, name_dir))
+                download_thread.start()
         else:
             messagebox.showinfo("Error!!!...", "Enter a URL for the playlis")
+            textbox.delete(0, tk.END)
     else:
-        messagebox.showinfo("Error!!!...", "Enter a URL")
-    textbox.delete(0, tk.END)
+        messagebox.showinfo("Error!!!...", "Enter a URL of Spotify")
 
 def get_name_music(playlist_id):
 
@@ -106,7 +99,9 @@ def get_name_music(playlist_id):
         # Imprime los nombres de las canciones
         for item in playlist['tracks']['items']:
             track = item['track']
-            list_name.append(track['name'])
+            #list_name.append(f"{track['name']} {track['artists'][0]['name']}")
+            #list_name.append(track['name'])
+            list_name.append(f"{track['name']} {track['artists'][0]['name']} Audio original")
         
         return list_name, name_dir
     except Exception as e:
@@ -129,7 +124,7 @@ def create_dir(name):
     except FileExistsError:
         new_name = ""
         while new_name == "":
-            new_name = simpledialog.askstring("New name", "Playlist exist\nEnter a new name for the directory.:")
+            new_name = simpledialog.askstring("New name", "The directory already exists\nEnter a new name for the directory:")
 
             if new_name == None or validate_name(new_name) != True:
                 messagebox.showinfo("Error!!!...", "Enter a valid name")
@@ -155,7 +150,7 @@ def validate_name(name):
     
     return True
 
-def download_music(name_music, name_dir):
+def download_music(name_music, name_dir, i, total):
     try:
         # Buscar videos relacionados con el nombre de la música en YouTube
         Search = VideosSearch(name_music, limit = 1)
@@ -166,10 +161,23 @@ def download_music(name_music, name_dir):
         url_video = result['result'][-1]['link']
 
         # Crear objeto YouTube
-        video = YouTube(url_video)
+        video = YouTube(url_video, on_progress_callback=on_progress)
+
+        if i == 0:
+            show_message()
+
+        textbox.config(state=tk.DISABLED)
+        btn_download.config(state=tk.DISABLED)
 
         # Seleccionar solo las corrientes de audio
         #video.streams.filter(only_audio=True).first().download(output_path=name_dir)
+        label_size.config(text=f"{0.0}MB - {round((video.streams.get_audio_only().filesize/1048576), 2)}MB {0:.0%} Completado")
+        label_count.config(text=f"{i+1} de {total} Canciones")
+        bar.pack()
+        label_size.pack()
+        label_D.config(text=f"Descargando '{video.title}.mp3'")
+        bar['value']=0
+
         video.streams.get_audio_only().download(output_path=name_dir)
         
         new_name_music = video.title
@@ -179,38 +187,82 @@ def download_music(name_music, name_dir):
                 new_name_music = new_name_music.replace(c, "")
 
         if os.path.isfile(name_dir+'\\'+new_name_music+'.mp3'):
-                os.remove(name_dir+'\\'+new_name_music+'.mp3')
+            os.remove(name_dir+'\\'+new_name_music+'.mp3')
         
         os.rename(os.path.join(name_dir, new_name_music+'.mp4'), os.path.join(name_dir, new_name_music+'.mp3'))
 
         text_area.config(state=tk.NORMAL) 
         text_area.tag_configure("colorG", foreground="green")
-        text_area.insert(tk.END,  f"> '{new_name_music}' -> Successful download.\n", "colorG")
+        text_area.insert(tk.END,  f"> '{new_name_music}.mp3' -> Successful download.\n", "colorG")
         text_area.config(state=tk.DISABLED)
 
+        if i == (total-1):
+            messagebox.showinfo("", "Descarga completa")
+            textbox.config(state=tk.NORMAL)
+            textbox.delete(0, tk.END)
+            label_D.config(text=f"")
+            label_count.config(text=f"")
+            label_size.config(text=f"")
+            bar['value']=0
+            bar.pack_forget()
+            btn_download.config(state=tk.NORMAL)
+
     except Exception as e:
-        text_area.config(state=tk.NORMAL) 
-        #text_area.insert(tk.END, f">'{result['result'][0]['title']}' fail download.\n'{e}'\n")
+        #messagebox.showinfo("", "Descarga incompleta")
+        if i == (total-1):
+            btn_download.config(state=tk.NORMAL)
+            textbox.config(state=tk.NORMAL)
+
+        text_area.config(state=tk.NORMAL)
         text_area.tag_configure("colorR", foreground="red")
         text_area.insert(tk.END, f">Fail download.\n'{e}'\n", "colorR")
         text_area.config(state=tk.DISABLED)
 
-def bar_progres(segment, total, long):
-    percentage = segment / total
+def on_progress(stream, chunk, bytes_remaining):
+    if alert.winfo_viewable() == 1:
+        for widget in alert.winfo_children():
+            widget.destroy()
+        alert.withdraw()
+    
+    total = stream.filesize #limite
+    segmen = total - bytes_remaining #segmento
+    long = 50
+
+    percentage = segmen / total
     complete = int(percentage * long)
-    missing = long - complete
-    bar = f"[{'#' * complete}{'-' * missing}]{percentage:.2%}"
-    return bar
+    bar['value']=(complete*2)
+    label_size.config(text=f"{round((segmen/1048576), 2)}MB - {round((total/1048576), 2)}MB {percentage:.0%} Completado")
+    window.update_idletasks()
+    #print(f"[{'=' * complete}{'-' * missing}]{percentage:.2%}", end = "\r")
+
+def show_message():
+    alert.deiconify() 
+    tk.Label(alert, text="starting download", font=("Bold", 14)).pack()
+
+def close_button():
+    pass 
 
 #Settings of window 
 window = tk.Tk()
+
+alert = tk.Toplevel(window)
+alert.title("Please wait...")
+alert.attributes('-toolwindow', True)
+alert.protocol("WM_DELETE_WINDOW", close_button)
+screen_width = alert.winfo_screenwidth()
+screen_height = alert.winfo_screenheight()
+x_position = (screen_width - 250) // 2
+y_position = (screen_height - 50) // 2
+alert.geometry("250x50+{}+{}".format(x_position, y_position))
+alert.withdraw()
+
 window.title("Download playlist")
 window.resizable(width=False, height=False)
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 x_position = (screen_width - 600) // 2
-y_position = (screen_height - 400) // 2
-window.geometry("600x400+{}+{}".format(x_position, y_position))
+y_position = (screen_height - 500) // 2
+window.geometry("600x500+{}+{}".format(x_position, y_position))
 
 #Settings of textbox
 label_btn = tk.Label(window, text="URL of playlist", font=("Bold", 14))
@@ -225,7 +277,27 @@ btn_download.pack(pady=10)
 text_area = tk.Text(window, height=15, width=60, state=tk.DISABLED)
 text_area.pack()
 
+label_D = tk.Label(window, text="", font=("Bold", 13))
+label_D.pack(pady=10)
+
+bar = Progressbar(window, length=500)
+
+label_count = tk.Label(window, text="", font=("Bold", 12))
+label_count.pack()
+
+label_size = tk.Label(window, text="", font=("Bold", 12))
+#label_size.pack()
+
 window.mainloop()
 
+#Los temerarios
 #https://open.spotify.com/playlist/1IzIMoFRUoLCg4GMkvmNPs?si=f28c40f6020a466b
+#The outfield
+#https://open.spotify.com/playlist/67qH0bKuwuKbqK2INYVOSz
+#Electronica
+#https://open.spotify.com/playlist/751fGb0bC9Zpcl6gElqTud
+#Carla Morison
+#https://open.spotify.com/playlist/5LsncNJlhYEmsdZqThRNg1
+#Banda
+#https://open.spotify.com/playlist/45egBgOmsmHcGfS3wK7qSM
 #http://localhost/?code=AQC7ozfoqYnHoWQiwrFbSEBFjkTTHz4-GM-j-lNAt7i6dftsmHUadvzEmgLx478tZeSoMHXBeLIAMqGXB4og3vPM8YyjAYdRPhbnvZV_8is0CMoWLEOGH_cVLK1vMAQ7WhKdfphvwxJBqQACgsUwVvzTEK03w__DA9DNzoS_PJRsXazOXiCNK3C0QA4
--- Spotify/Youtube.py
+++ Spotify/Youtube.py
@@ -1,27 +1,22 @@
 # Importamos la librería tkinter para la interfaz grafica
 import tkinter as tk
 from tkinter import messagebox
+from tkinter.ttk import *
 # Importamos la libreria urllib.parse para verificar las URL's ingresadas
 from urllib.parse import urlparse
 # Importamos la librería pytube para descargar videos de YouTube
 from pytube import YouTube
 import os
-import platform
-import sys
 import threading
 
 def is_url(url):
     try:
         result = urlparse(url)
-        return all([result.scheme, result.netloc])
+        return result.netloc in {"www.youtube.com", "youtube.com", "youtu.be"}
     except ValueError:
         return False
     
 def get_URL():
-    if platform.system() == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
     #Pedimos al usuario el link de su playlist de Spotify
     video_link = textbox.get()
     text_area.delete("1.0", tk.END)
@@ -38,40 +33,30 @@ def get_URL():
                 if video_id[0] == 'watch':
                     option = var.get()
                     if option > 0:
-                        i=1
-                        limit = 100
-                        #print("\nDownloading")
-                        #bar = bar_progres(0, limit, 50)
-                        #print(f"{bar}", end = "\r")
-                        #download_thread = threading.Thread(target=download_music, args=(video_link, option))
-                        #download_thread.start()
-                        print("Downloading")
-                        download_music(video_link, option)
-                        #print()
-                        #bar = bar_progres(i, limit, 50)
-                        #print(f"{bar}", end = "\r")
-                        #i += 1
-
-                        textbox.delete(0, tk.END)
-                        #print("\n\nDownload complete\n")
+                        bar['value']=0
+                        download_thread = threading.Thread(target=download_music, args=(video_link, option))
+                        download_thread.start()
                     else:
-                        messagebox.showinfo("Error!!!...", "Select a option")
+                        messagebox.showinfo("Error!!!...", "Seleccione una opción")
                 else:
-                    messagebox.showinfo("Error!!!...", "Enter a valid URL")
+                    messagebox.showinfo("Error!!!...", "Ingrese una URL válida")
+                    textbox.delete(0, tk.END)
         else:
-            messagebox.showinfo("Error!!!...", "Enter a URL for the watch")
+            messagebox.showinfo("Error!!!...", "Ingrese una URL de video")
+            textbox.delete(0, tk.END)
     else:
-        messagebox.showinfo("Error!!!...", "Enter a URL")
-    textbox.delete(0, tk.END)
-    var.set(0)
+        messagebox.showinfo("Error!!!...", "Ingrese una URL de YouTube")
+        textbox.delete(0, tk.END)
 
 def download_music(url_video, option):
     try:
         # Crear objeto YouTube
-        #on_progress_callback=progres
         video = YouTube(url_video, on_progress_callback=on_progress)
 
-        #print(video.streams.get_audio_only().filesize)
+        show_message()
+        textbox.config(state=tk.DISABLED)
+        btn_download.config(state=tk.DISABLED)
+
         # Ruta completa de la carpeta de Descargas usando la variable de entorno
         download_path = os.path.join(os.path.expanduser("~"), "Downloads")
 
@@ -83,6 +68,10 @@ def download_music(url_video, option):
 
         extens = ''
         if option == 1:
+            label_size.config(text=f"{0.0}MB - {round((video.streams.get_audio_only().filesize/1048576), 2)}MB {0:.0%} Completado")
+            bar.pack()
+            label_D.config(text=f"Descargando '{video.title}.mp3'")
+
             video.streams.get_audio_only().download(output_path=download_path)
 
             if os.path.isfile(download_path+'\\'+new_name_music+'.mp3'):
@@ -92,6 +81,10 @@ def download_music(url_video, option):
             type = "Audio"
             extens = '.mp3'
         else:
+            label_size.config(text=f"{0.0}MB - {round((video.streams.get_highest_resolution().filesize/1048576), 2)}MB {0:.0%} Completado")
+            bar.pack()
+            label_D.config(text=f"Descargando '{video.title}.mp4'")
+
             if os.path.isfile(download_path+'\\'+new_name_music+'.mp4'):
                 os.remove(download_path+'\\'+new_name_music+'.mp4')
 
@@ -101,45 +94,76 @@ def download_music(url_video, option):
 
         text_area.config(state=tk.NORMAL)
         text_area.tag_configure("colorG", foreground="green")
-        text_area.insert(tk.END,  f"> {type}: '{video.title}{extens}' -> successful download.\n", "colorG")
+        text_area.insert(tk.END,  f"> {type}: '{video.title}{extens}' -> Descarga exitosa.\n", "colorG")
         text_area.config(state=tk.DISABLED)
+        
+        messagebox.showinfo("", "Descarga completa")
+        textbox.config(state=tk.NORMAL)
+        textbox.delete(0, tk.END)
+        label_D.config(text=f"")
+        label_size.config(text=f"")
+        bar['value']=0
+        bar.pack_forget()
+        var.set(0)
+        btn_download.config(state=tk.NORMAL)
 
     except Exception as e:
+        messagebox.showinfo("", "Descarga incompleta")
+        btn_download.config(state=tk.NORMAL)
+        textbox.config(state=tk.NORMAL)
         text_area.config(state=tk.NORMAL) 
         text_area.tag_configure("colorR", foreground="red")
-        text_area.insert(tk.END, f"> Fail download -> {e}\n", "colorR")
+        text_area.insert(tk.END, f"> Fallo en la descarga -> {e}\n", "colorR")
         text_area.config(state=tk.DISABLED)
 
 def on_progress(stream, chunk, bytes_remaining):
-    def print_progress():    
-        total = stream.filesize #limite
-        segmen = total - bytes_remaining #segmento
-        long = 50
-
-        percentage = segmen / total
-        complete = int(percentage * long)
-        missing = long - complete
-
-        print(f"[{'=' * complete}{'-' * missing}]{percentage:.2%}", end = "\r")
+    if alert.winfo_viewable() == 1:
+        for widget in alert.winfo_children():
+            widget.destroy()
+        alert.withdraw()
     
-    progress_thread = threading.Thread(target=print_progress)
-    progress_thread.start()
-    
-    #sys.stdout.flush()
+    total = stream.filesize #limite
+    segmen = total - bytes_remaining #segmento
+    long = 50
 
+    percentage = segmen / total
+    complete = int(percentage * long)
+    bar['value']=(complete*2)
+    label_size.config(text=f"{round((segmen/1048576), 2)}MB - {round((total/1048576), 2)}MB {percentage:.0%} Completado")
+    window.update_idletasks()
+    #print(f"[{'=' * complete}{'-' * missing}]{percentage:.2%}", end = "\r")
+
+def show_message():
+    alert.deiconify()
+    tk.Label(alert, text="Iniciando descarga", font=("Bold", 14)).pack()
+
+def close_button():
+    pass 
 
 #Settings of window 
 window = tk.Tk()
-window.title("Download music")
+
+alert = tk.Toplevel(window)
+alert.title("Por favor espere")
+alert.attributes('-toolwindow', True)
+alert.protocol("WM_DELETE_WINDOW", close_button)
+screen_width = alert.winfo_screenwidth()
+screen_height = alert.winfo_screenheight()
+x_position = (screen_width - 250) // 2
+y_position = (screen_height - 50) // 2
+alert.geometry("250x50+{}+{}".format(x_position, y_position))
+alert.withdraw()
+
+window.title("Descarga de música y video")
 window.resizable(width=False, height=False)
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 x_position = (screen_width - 600) // 2
-y_position = (screen_height - 400) // 2
-window.geometry("600x400+{}+{}".format(x_position, y_position))
+y_position = (screen_height - 450) // 2
+window.geometry("600x460+{}+{}".format(x_position, y_position))
 
 #Settings of textbox
-label_btn = tk.Label(window, text="URL of video YouTube", font=("Bold", 14))
+label_btn = tk.Label(window, text="URL de YouTube", font=("Bold", 14))
 label_btn.pack(pady=10)
 textbox = tk.Entry(window, width=65, font=("Calibri", 11))
 textbox.pack(pady=1)
@@ -156,8 +180,16 @@ text_area = tk.Text(window, height=10, width=65, state=tk.DISABLED)
 text_area.pack()
 
 #Settings of Button
-btn_download = tk.Button(window, text="Download", font=("Bold", 12), command=get_URL)
+btn_download = tk.Button(window, text="Descargar", font=("Bold", 12), command=get_URL)
 btn_download.pack(pady=10)
+
+label_D = tk.Label(window, text="", font=("Bold", 13))
+label_D.pack(pady=10)
+
+bar = Progressbar(window, length=500)
+
+label_size = tk.Label(window, text="", font=("Bold", 12))
+label_size.pack()
 
 window.mainloop()
 
